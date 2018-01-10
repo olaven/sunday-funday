de003d383c4b5b3df13ee3ba860aea9beb4c9608
olaven <olavsundfoer@gmail.com>
2018-01-11 00:10:14 +0100

    added update to page

--- langtonsAnt/sketch.js
+++ langtonsAnt/sketch.js
@@ -1,102 +1,52 @@
 
 //user desides the steps per frame -> 1 by default
-let stepsPerFrame;
+let stepsPerFrame; //assigned in separate script (index.html)
+let amountOfAnts; //assigned in separate script (index.html)
 //grid (2DArray)
 let grid;
-//position of ant
-let x;
-let y;
+//array of ant-objects
+let ants;
 //direction of ant
-let dir;
-//possible directions
-const UP = 0;
-const RIGHT = 1;
-const DOWN = 2;
-const LEFT = 3;
+
 
 function setup() {
   createCanvas(floor(windowWidth), floor(windowHeight));
   background(200);
+  //if not assigned by input
   stepsPerFrame = 1;
+  amountOfAnts = 1;
+
   grid = make2DArray(width, height);
-  x = floor(width / 2);
-  y = floor(height / 2);
-  //place ant
-  grid[x][y] = 1;
-  //starting direction
-  dir = UP;
+
+  ants = new Array();
+  //adding ants to array
+  addAnt(amountOfAnts, ants);
 }
 function draw() {
   //DENNE LOOPER AV SEG SELV -> p5 being p5
-  movementLogic();
-  let counter = 0;
-  while(counter < stepsPerFrame){//user defines amount of steps per frame
-    movementLogic();
-    counter++;
+  for(i in ants){
+    let counter = 0;
+    while(counter < stepsPerFrame){//user defines amount of steps per frame
+      movementLogic(ants[i]);
+      counter++;
+    }
   }
 }
 //the movement logic, implemented in each draw()
-function movementLogic() {
-  if(grid[x][y] == 0) {
-    turnRight();
-    grid[x][y] = 1;
-  } else if (grid[x][y] == 1) {
-    turnLeft();
-    grid[x][y] = 0;
+function movementLogic(ant) {
+  if(grid[ant.x][ant.y] == 0) {
+    ant.turnRight();
+    grid[ant.x][ant.y] = 1;
+  } else if (grid[ant.x][ant.y] == 1) {
+    ant.turnLeft();
+    grid[ant.x][ant.y] = 0;
   }
-  stroke(color(255 * grid[x][y]));
-  moveForward();
-  point(x ,y);
+  stroke(color(255 * grid[ant.x][ant.y]));
+  ant.moveForward();
+  point(ant.x ,ant.y);
 }
 
-//move forward
-function moveForward() {
-  //change direction
-  switch (dir) {
-    case UP:
-      y--;
-      break;
-    case RIGHT:
-      x += 1;
-      break;
-    case DOWN:
-      y++;
-      break;
-    case LEFT:
-      x -= 1;
-      break;
-    default:
-      throw "invalid direction";
-  }
-  //check if the ant is about to move outside of borders
-  if(y < 0){
-    y = height - 1;
-  } else if (y > height - 1) {
-    y = 0;
-  }
 
-  if (x < 0) {
-    x = width - 1;
-  } else if (x > width - 1) {
-    x = 0;
-  }
-}
-//turning
-function turnRight() {
-  //turn clockwize
-  if((dir + 1) > LEFT){
-    dir = UP;
-  } else {
-    dir++;
-  }
-}
-function turnLeft() {
-  //turn agains the clock
-  dir--;
-  if(dir < UP){
-    dir = LEFT;
-  }
-}
 //returns a 2d array with specified "rows" and "columns"
 function make2DArray(rows, columns){
   let returArr = [];
@@ -107,4 +57,13 @@ function make2DArray(rows, columns){
     }
   }
   return returArr;
+}
+
+function addAnt(amount, arr) {
+  for(let i = 0; i < amount; i++){
+    let x = floor(random(1, width - 1));
+    let y = floor(random(1, height - 1));
+
+    arr.push(new Ant(x, y));
+  }
 }
