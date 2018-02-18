8a96587dfa45b380f6f8f775fd55153f5636a1c8
olaven <olavsundfoer@gmail.com>
2018-02-18 18:27:16 +0100

    will contain logic for adding, removing and listing projects from projects.json

---
+++ projectsManager.js
@@ -0,0 +1,13 @@
+//TODO: Functionality not implemented yet 
+module.exports = {
+    add : () => {
+        console.log("enter info about the project"); 
+    }, 
+    remove : () => {
+        //NOTE: Make sure that there can't be two projects with the same name
+        console.log("what is the name of the project you want to remove?"); 
+    },
+    list : () => {
+        console.log("displaying a list of all projects"); 
+    }
+}
