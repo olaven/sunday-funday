bbd095868b181912ab2712a0d8209e67affdfc04
olaven <olavsundfoer@gmail.com>
2018-02-28 21:33:08 +0100

    waiting with API integration until I find a good way to store the key. Started work on Sun and skycolor, which I am quite happy with

---
+++ projects/weatherAnimation/Sun.js
@@ -0,0 +1,43 @@
+function Sun(){
+    this.angle = 180; 
+    this.x; 
+    this.y;
+
+    
+    this.move = () => {
+        this.angle = (this.angle % 360) + 1 / 200; 
+    }
+    this.draw = () => {
+        //point the sun revolves around 
+        let centerX = width / 2; 
+        let centerY = height; 
+        let r = height / 1.5; 
+        //the x and y pos of the sun based on current angle 
+        this.setX(r * cos(this.angle) + centerX); 
+        this.setY(r * sin(this.angle) + centerY); 
+
+        //draw new sun
+        ellipse(this.x, this.y, width/5, width/5);
+    }
+    /**
+     * 
+     * @param {number} x sets the x-position of sun
+     */
+    this.setX = x => {
+        this.x = x;
+    };
+    /**
+     * 
+     * @param {number} y sets the y-position of sun
+     */
+    this.setY = y => {
+        this.y = y;
+    };
+    /**
+     * 
+     * @param {number} angle sets the angle in relation to center  
+     */
+    this.setAngle = (angle) => {
+        this.angle = angle; 
+    }
+}
--- projects/weatherAnimation/sketch.js
+++ projects/weatherAnimation/sketch.js
@@ -1,4 +1,5 @@
 let raindrops;
+let sun; 
 
 let weatherdata; 
 
@@ -7,22 +8,43 @@ let url;
 
 //preload runs before setup() and draw() 
 function preload(){
+  key = "get key later at open weather maps "
   url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=" + key;
-  weatherdata = loadJSON(url); 
-  console.log(weatherdata); 
 }
 //runs before first draw and prepares the state
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1000, 1000);
 
-  raindrops = populateWithObject(40, Raindrop);
+  raindrops = populateWithObject(100, Raindrop);
+  sun = new Sun();  
 }
 //runs for each frame 
 function draw() {
-  background(0,0,0);
-
-  if (true) {
+  
+  if(true){//adjust backgrond if sun is not visble 
+    applyCyclus(); 
+  }
+  if (true) { //if raining TODO: add snow and wind (mov x)
     applyRain();
+  }
+  if(true) { //if sun
+    applySun(); 
+  }
+  if (false) { //if cloudy
+    applyClouds(); 
+  }
+}
+
+/** 
+ * displays different sky colors depending on suns position (TODO: time of day)
+*/
+let applyCyclus = () => {
+  console.log("height:" + height); 
+  console.log("y     :" + sun.y); 
+  if (sun.y > height || sun.x > width + width/5) {
+    background("#001848"); 
+  } else {
+    background("#87CEEB"); 
   }
 }
 
@@ -35,6 +57,23 @@ let applyRain = () => {
     raindrop.draw();
   }
 };
+
+/**
+ * displays a sun, moving across the sky and back (TODO based on time of day)
+ */
+let applySun = () => {
+  sun.move(); 
+  sun.draw(); 
+}
+
+/**
+ * displays clouds, moving across the sky and back
+ */
+let applyClouds = () => {
+  //TODO: implement
+}
+
+
 
 /**
  * returns array populated with objects of specified type
